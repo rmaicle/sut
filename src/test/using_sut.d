f51d4aa7ec00ee5aba6658aa0be222db2f914099
rmaicle <rmaicle@gmail.com>
2021-03-31 23:30:56 +0800

    Fix encapsulating module

--- src/test/using_sut.d
+++ src/test/using_sut.d
@@ -1,22 +1,24 @@
 /**
- * This module is intended to encapsulate the external `sut` module.
+ * This module is only to conditionally compile-in the use of `sut` module.
  */
-module using_sut;                                       // NOTE
+module using_sut;                                                       // NOTE
 
-version (unittest):
-
-/**
- * Conditionally compile-in the `sut` module.
- */
-static if (__traits(compiles, { import sut; })) {
-    public import sut;
-}
-
-/**
- * Unit test block prologue code.
- */
-enum prologue=`
+version (sut) {
+    /**
+     * Conditionally compile-in the `sut` module.
+     */
     static if (__traits(compiles, { import sut; })) {
-        mixin (using_sut.unitTestBlockPrologue());      // NOTE
+        public import sut;
     }
-`;
+
+    /**
+     * Unit test block prologue code.
+     */
+    enum prologue=`
+        static if (__traits(compiles, { import sut; })) {
+            mixin (using_sut.unitTestBlockPrologue!(__LINE__ - 2)());   // NOTE
+        }
+    `;
+} else {
+    enum prologue="";
+}
