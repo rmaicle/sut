1fcf141ddf2a1552f49c388d7a854bb4cc940e50
rmaicle <rmaicle@gmail.com>
2021-05-24 00:00:18 +0800

    Move fail count block out of custom assert handler
    This block was moved out of the custom assert handler because
    an assertion in a function that is called from a unit test
    satisfies the condition and reports a failure even though the
    unit test has caught the assertion via assertThrown.

--- src/sut/runner.d
+++ src/sut/runner.d
@@ -93,6 +93,13 @@ customUnitTestRunner ()
                 printAssertion(m.name, e);
                 printTrace(e);
             }
+            // This block was moved out of the custom assert handler because
+            // an assertion in a function that is called from a unit test
+            // satisfies the condition and reports a failure even though the
+            // unit test has caught the assertion via assertThrown.
+            if (unitTestCounter.unitTestBlock.isIn()) {
+                unitTestCounter.current.revertPassing();
+            }
         }
         if (unitTestCounter.current.isSomeExecuted()) {
             printModuleSummary(
@@ -274,8 +281,5 @@ customAssertHandler (
     string msg = string.init
 ) {
     import core.exception: AssertError;
-    if (unitTestCounter.unitTestBlock.isIn()) {
-        unitTestCounter.current.revertPassing();
-    }
     throw new AssertError(msg, file, line);
 }
